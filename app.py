94d8ffc4446aea867d1aa4a23f5b2a9fdad8bcd9
Manish <jon.manish.adh@gmail.com>
2018-11-19 13:41:19 +0545

    user login/logout facility added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect,url_for,session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_session import Session
 
 engine = create_engine("postgres://tbhpfvawuvwbop:5eec2e3c8ceb5f4ba9483fbf1ef94577a63c3e54e812afb6ed651a3d7b647345@ec2-54-204-46-60.compute-1.amazonaws.com:5432/d18j3m18dv9b70")
 
@@ -8,17 +10,58 @@ db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route('/')
 def index():
     return render_template("layout.html")
 
-@app.route('/login')
+@app.route('/login',methods=["POST","GET"])
 def login():
+    if request.method == 'POST':
+        usrname = request.form.get("username")
+        paswrd = request.form.get("password")
+        #paswrd = generate_password_hash(paswrd)
+        user = db.execute("SELECT * FROM users WHERE username=:user AND password=:password",{'user':usrname,'password':paswrd}).fetchone()
+        if user is None:
+            return render_template("login.html",message="User doesnot exist")
+        session['user_id']= user.id
+        return redirect(url_for("profile"))
+    
     return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=["POST","GET"])
 def signup():
+    if request.method == 'POST':
+        usrname = request.form.get("reg_username")
+        paswrd = request.form.get("reg_password")
+        email = request.form.get("reg_email")
+        paswrd = generate_password_hash(paswrd)
+        try:
+            db.execute("INSERT INTO users(username,password,email) VALUES(:username,:password,:email)",{"username":usrname,"password":paswrd,"email":email})
+            db.commit()
+            return render_template("sucess.html",username=usrname)
+        except:
+            return render_template("signup.html",username=usrname)
     return render_template("signup.html")
+
+@app.route('/profile')
+def profile():
+    if 'user_id' in session:
+        books = db.execute("SELECT * FROM books ORDER BY RANDOM() LIMIT 15").fetchall()
+        user=db.execute("SELECT * FROM users WHERE id=:id", {"id":session["user_id"]}).fetchone()
+        return render_template("profile.html",books=books,user=user)
+
+    return "<h1>Invalid Entry</h1>";
+
+@app.route('/logout')
+def logout():
+    if session['user_id'] != None:
+        del session['user_id']
+
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -17,27 +17,12 @@
 	// Validation
   $("#login-form").validate({
   	rules: {
-      lg_username: "required",
-  	  lg_password: "required",
+      username: "required",
+  	  password: "required",
     },
   	errorClass: "form-invalid"
   });
   
-	// Form Submission
-  $("#login-form").submit(function() {
-  	remove_loading($(this));
-		
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-		
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
 	
 	// Register Form
 	//----------------------------------------------
@@ -70,87 +55,4 @@
   	  }
     }
   });
-
-  // Form Submission
-  $("#register-form").submit(function() {
-  	remove_loading($(this));
-		
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-		
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
-
-	// Forgot Password Form
-	//----------------------------------------------
-	// Validation
-  $("#forgot-password-form").validate({
-  	rules: {
-      fp_email: "required",
-    },
-  	errorClass: "form-invalid"
-  });
-  
-	// Form Submission
-  $("#forgot-password-form").submit(function() {
-  	remove_loading($(this));
-		
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-		
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
-
-	// Loading
-	//----------------------------------------------
-  function remove_loading($form)
-  {
-  	$form.find('[type=submit]').removeClass('error success');
-  	$form.find('.login-form-main-message').removeClass('show error success').html('');
-  }
-
-  function form_loading($form)
-  {
-    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
-  }
-  
-  function form_success($form)
-  {
-	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
-	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success']);
-  }
-
-  function form_failed($form)
-  {
-  	$form.find('[type=submit]').addClass('error').html(options['btn-error']);
-  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
-  }
-
-	// Dummy Submit Form (Remove this)
-	//----------------------------------------------
-	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
-  function dummy_submit_form($form)
-  {
-  	if($form.valid())
-  	{
-  		form_loading($form);
-  		
-  		setTimeout(function() {
-  			form_success($form);
-  		}, 2000);
-  	}
-  }
-	
 })(jQuery);
