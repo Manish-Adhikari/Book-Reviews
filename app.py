463d97380e14e4308e0539884e273af45e18f4fe
Manish <jon.manish.adh@gmail.com>
2018-11-20 20:34:15 +0545

    removed password encryption

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect,url_for,session, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
